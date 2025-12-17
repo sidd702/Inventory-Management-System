06c94c896aeff07280d3171214f723cae3650672
Siddharth Shah <siddshah702@gmail.com>
2025-12-17 17:47:00 +0530

    Added Order Service

---
+++ order_service/src/main/java/com/sidd/order_service/OrderServiceApplication.java
@@ -0,0 +1,14 @@
+package com.sidd.order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+        System.out.println("OrderServiceApplication is running on Port 8082");
+	}
+
+}
